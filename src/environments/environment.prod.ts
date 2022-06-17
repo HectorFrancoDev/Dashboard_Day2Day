81a134913a53a607acab0b7ffdb61a5efa762eab
Hector Franco <hector.francomasmela@davivienda.com>
2022-06-17 10:03:04 -0500

    Google Auth ID

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,5 +6,6 @@ export const environment = {
   // API_URL: 'https://backend-time.herokuapp.com/api',
   API_URL: "http://localhost:5000/api",
 
-  GOOGLE_CLIENT_ID: '29975082214-e04ogm4bv64rjh68ldhat690slf4u8du.apps.googleusercontent.com',
+  // GOOGLE_CLIENT_ID: '29975082214-e04ogm4bv64rjh68ldhat690slf4u8du.apps.googleusercontent.com',
+  GOOGLE_CLIENT_ID: '934028073134-sse87thgo51ahl54epnb0o02js8karps.apps.googleusercontent.com',
 };
